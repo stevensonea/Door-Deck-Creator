396a1018c569f1d71acf04ea70df40ce5d6c8bbb
stevenea <stevenea@iu.edu>
2023-08-11 13:29:14 -0400

    added font to workspace

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from PIL import ImageFont
 
 image = Image.open('pictures/creeper.jpeg')
 print(image.getbbox())
-font = ImageFont.truetype('MinecraftTen-VGORe.ttf', 100)
+font = ImageFont.truetype('fonts/MinecraftTen-VGORe.ttf', 200)
 
 I1 = ImageDraw.Draw(image)
-I1.text((image.getbbox()[2] / 2, 50), "Ethan", font = font, fill = (0, 0, 0))
+I1.text((image.getbbox()[2] / 6, image.getbbox()[3] / 2.5), "Ethan", font = font, fill = (0, 0, 0))
 image.show()
